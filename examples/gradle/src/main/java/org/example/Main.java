3e99f6bec532d5071a2c4a67cac0a490f290171d
Sergey Saprikin <ssaprikin@cloudlinux.com>
2024-08-02 15:55:42 +0300

    Changing documentation

---
+++ examples/gradle/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
